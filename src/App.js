9c5e0c62b990cc6cc2f5e78695d831e88d710dc6
benarcetinbilek <bcetinbilek@hotmail.com>
2022-03-11 23:43:46 +0000

    new

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ export default function App() {
   return (
     <div className="App">
       <button onClick={() => setShow(true)}>Show Modal</button>
+      <div>
+        
+      </div>
       {show ? <Modal setShow={setShow} title="My Modal" onClose={() => setShow(false)} show={show}>
           <p>This is modal body</p>
-        </Modal> : <></>}
-        
+        </Modal> : <></>}        
     </div>
   );
 }
